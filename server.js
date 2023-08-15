f4d2a76c88b0c36c21fc16906ae196816308c4d2
sraveshnandan <kumarsravesh39@gmail.com>
2023-08-15 17:14:01 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const axios = require("axios");
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    // Get or create user on Chat Engine!
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "Private-Key": "09924007-0425-4d9d-95d2-a230894d6639" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
+});
+
+app.listen(3001);
